8b770d16bcbefd1e58b32833776599d6b69d8a85
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-08-02 09:27:49 +0530

    sprint1

--- src/test/java/com/jap/PhLevelAnalyserTest.java
+++ src/test/java/com/jap/PhLevelAnalyserTest.java
@@ -2,16 +2,36 @@ package com.jap;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PhLevelAnalyserTest {
+    PhLevelAnalyser object;
 
     @Before
     public void setUp()  {
+        object = new PhLevelAnalyser();
+    }
+    @Test
+    public void getPhValurOfWaterForInputPhSeven(){
+        String actual = object.getPhValueOfWater(7);
+        assertEquals("pH value is fine",actual);
     }
 
+    @Test
+    public void getPhValurOfWaterForInputPhEight(){
+        String actual = object.getPhValueOfWater(8);
+        assertEquals("pH value is fine",actual);
+    }
+
+    @Test
+    public void getPhValurOfWaterForInputPhTen(){
+        String actual = object.getPhValueOfWater(10);
+        assertEquals("pH value is high, partial water change required",actual);
+    }
     @After
     public void tearDown()  {
+        object=null;
     }
 }
